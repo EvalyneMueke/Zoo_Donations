5c75c59bcc2d39b34a8f81cb884b52c24e4ae60b
Evalyne <evalyne.mueke@student.moringaschool.com>
2022-12-23 17:49:26 +0300

    Made Search Bar Functional

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-document.addEventListener('DOMContentLoaded',() =>{
+document.addEventListener("DOMContentLoaded", () => {
+  //Event Listeners
+  document
+    .querySelector("#animal-form")
+    .addEventListener("submit", handleSubmit);
 
+  //EventHandlers
+  function handleSubmit(e) {
+    e.preventDefault();
+    let animalObj = {
+      animal: e.target.animal.value,
+      image: e.target.image.value,
+      fact: e.target.fact.value,
+      donation: 0,
+    };
 
+    renderAnAnimal(animalObj);
+    adoptAnimal(animalObj);
+  }
 
+  //Intial call
 
-//Event Listeners
-document.querySelector("#animal-form").addEventListener('submit',
-handleSubmit)
-
-//EventHandlers
-function handleSubmit(e){
-    e.preventDefault()
-    let animalObj ={
-        animal:e.target.animal.value,
-        image:e.target.image.value,
-        fact:e.target.fact.value,
-        donation: 0
-    }
-    
-    renderAnAnimal(animalObj)
-    adoptAnimal(animalObj)
-}
-
-//Intial call
-
-function renderAnAnimal(animal) {
+  function renderAnAnimal(animal) {
     //Making a card for an animal
-    let card = document.createElement('li')
-    card.className='card'
-    card.innerHTML =`
+    let card = document.createElement("li");
+    card.className = "card";
+    card.innerHTML = `
     <div class= "content">
     <img src="${animal.image}">
     
@@ -54,142 +51,135 @@ function renderAnAnimal(animal) {
  <br>
     </form>
     </div>
-    `
+    `;
 
     //add the animal card to the Dom
-    document.querySelector('#animal-list').appendChild(card)
+    document.querySelector("#animal-list").appendChild(card);
 
+    //Add an event listener to every donate button which will be used during updating the donations
+    //Increment the donations by 200 everytime it is clicked
+    card.querySelector("#donate").addEventListener("click", () => {
+      animal.donation += 200;
+      card.querySelector("span").textContent = animal.donation;
 
+      updateDonation(animal);
+    });
+    //Add event listener to the Send to park button to delete animal from zoo
+    card.querySelector("#set_free").addEventListener("click", () => {
+      card.remove();
+      deleteAnimal(animal.id);
+    });
+  }
 
- //Add an event listener to every donate button which will be used during updating the donations
- //Increment the donations by 200 everytime it is clicked
-    card.querySelector("#donate").addEventListener("click",() =>{
-        animal.donation +=200
-        card.querySelector('span').textContent =animal.donation
+  //FUNTIONS
 
+  //Function to fetch data from BigFive
+  function getAnimals() {
+    fetch("http://localhost:3000/BigFive")
+      .then((res) => res.json())
+      .then((BigFive) => BigFive.forEach((animal) => renderAnAnimal(animal)));
+  }
+  //POST request to add a new animal that is adopted to the zoo through the form
 
-
-        updateDonation(animal)
-
+  function adoptAnimal(animalObj) {
+    fetch("http://localhost:3000/BigFive", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(animalObj),
     })
-//Add event listener to the Send to park button to delete animal from zoo
-card.querySelector("#set_free").addEventListener("click",()=>{
-    card.remove()
-    deleteAnimal(animal.id)
+      .then((res) => res.json())
+      .then((animal) => console.log(animal));
+  }
+  //Patch request to update the donation amount
 
-})
+  function updateDonation(animalObj) {
+    updateDonation.preventDefault();
 
-
-    
-}
-
-
-//FUNTIONS
-
-//Function to fetch data from BigFive
- function getAnimals(){
-  fetch('http://localhost:3000/BigFive')
-  .then(res =>res.json())
-  .then(BigFive => BigFive.forEach(animal =>renderAnAnimal(animal)))
-
- }
-//POST request to add a new animal that is adopted to the zoo through the form
-
- function adoptAnimal(animalObj){
-   
-    fetch('http://localhost:3000/BigFive',{
-        method:'POST',
-        headers:{
-           'Content-Type':'application/json' 
-        },
-        body:JSON.stringify(animalObj)
+    fetch(`http://localhost:3000/BigFive/${animalObj.id}`, {
+      method: "PATCH",
+      header: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(animalObj),
     })
-    .then(res => res.json())
-    .then(animal =>console.log(animal) )
+      .then((res) => res.json())
+      .then((animal) => console.log(animal));
+  }
 
- }
- //Patch request to update the donation amount
+  //Delete to be set on set free
 
-function updateDonation(animalObj){
-    updateDonation.preventDefault()
-    
-    fetch(`http://localhost:3000/BigFive/${animalObj.id}`,{
-        method:'PATCH',
-        header:{
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(animalObj)
-
+  function deleteAnimal(id) {
+    fetch(`http://localhost:3000/BigFive/${id}`, {
+      method: "DELETE",
+      header: {
+        "Content-Type": "application/json",
+      },
     })
-    .then(res =>res.json())
-    .then(animal =>console.log(animal))
-}
+      .then((res) => res.json())
+      .then((animal) => console.log(animal));
+  }
 
-
-//Delete to be set on set free
-
-function deleteAnimal(id){
-
-    
- fetch(`http://localhost:3000/BigFive/${id}`,{
-    method:'DELETE',
-    header:{
-        'Content-Type':'application/json'
-    }
- })
- .then(res=>res.json())
- .then(animal =>console.log(animal))
-}
- 
-
-
- function intialize(){
-    getAnimals()
+  function intialize() {
+    getAnimals();
     //BigFive.forEach(animal =>renderAnAnimal(animal))
- }
- intialize()
+  }
+  intialize();
 
-
- //Searchbar
- var search = document.createElement('div');
- search.innerHTML = '<input type="text" id="search" placeholder="Search Animal Here">';
- document.querySelector('#search').appendChild(search);
- var searchResults = document.createElement('div');
- searchResults.innerHTML = '<ul id="searchResults"></ul>';
- document.body.appendChild(searchResults);
- var search = document.getElementById('search');
- var searchResults = document.getElementById('searchResults');
- search.addEventListener('keyup', function(e) {
-   var searchValue = e.target.value;
-   var searchResults = document.getElementById('searchResults');
-   searchResults.innerHTML = '';
-   var searchResultsList = document.createElement('ul');
-   searchResults.appendChild(searchResultsList);
-   var searchResultsListItem = document.createElement('li');
-   searchResultsList.appendChild(searchResultsListItem);
-   searchResultsListItem.innerHTML = 'http://localhost:3000/BigFive/' + searchValue ;
- });
-
-
- //Add alert upon submitting a comment
-
-//  let submit =document.querySelector('#submit')
-//  let comment_area=document.querySelector('#comment-area')
-//  submit.addEventListener('click',checkValidComment())
-
- 
-//  function checkValidComment(){
-
-//     if (comment_area.input !== " ") {
+  function SearchName() {
+    const form = document.getElementById("searchForm");
+    const input = document.getElementById("name").value;
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      fetch(`http://localhost:3000/BigFive?animal=${input}`)
+        .then((resp) => resp.json())
+        .then((data) => {
+          if (data.length != 0) {
+            const resultsContainer =
+              document.getElementById("resultsContainer");
+            resultsContainer.className = "card";
+            for (item of data) {
+              const resultDetails = `
+                    <img src="${item.image}">
     
-//         return alert("Enter a valid comment")
-//     }
-//     else{
-//         alert("Thank you for the comment")
-//     }
-//  }
-
-
-
-})
-
+    <h4>${item.animal}</h4>
+    <br>
+    <p>
+   KES<span class ="donation-count">${item.donation}</span> Donated
+   <p>
+   <br>
+   <p>${item.fact}</p>
+   <br>
+   <form>
+   <label>Your Name</label>
+   <br>
+   <input type="text">
+   <button class="button" id="donate" type="button" >Donation KES200</button>
+   <br>
+ <label>Reason To send To a Park</label>
+    <br>
+    <textarea></textarea>
+    <br>
+    <button class ="button" id="set_free" type="button">Send to The Park</button>
+ <br>
+ <br>
+    </form>
+    </div>
+                    `;
+              resultsContainer.innerHTML = resultDetails;
+            }
+          }
+          else{
+            const resultsContainer=document.getElementById('resultsContainer')
+            resultsContainer.className='card'
+            const notFoundMessage =`<h2> Sorry,Animal not in  the zoo</h2>`
+            resultsContainer.innerHTML=notFoundMessage
+          }
+          form.reset()
+        });
+    });
+  }
+  const searchButton =document.getElementById('searchButton')
+  searchButton.addEventListener("click",SearchName)
+});
